002d88ec83bb287c0f9724035ea4b148b7c368c4
it24101671 <it24101671@my.sliit.lk>
2025-09-16 23:48:48 +0530

    IT24101671_lab6

---
+++ Lab Sheet 06/IT24101671.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\Mumthas\\OneDrive\\Desktop\\IT24101671")
+
+punif(25,min=0,max=40)-punif(10,min=0,max=40)
+
+pexp(2,rate=1/3)
+
+pnorm(130,mean=100,sd=15,lower.tail = FALSE)
+
+qnorm(0.95,mean=100,sd=15)
